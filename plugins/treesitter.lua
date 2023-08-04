f6a87ca7396820e66668dfc65f6d81512f1d7aeb
Vincent550102 <5020559@gmail.com>
2023-08-04 15:02:38 +0800

    update javascript to treesitter

--- plugins/treesitter.lua
+++ plugins/treesitter.lua
@@ -1,6 +1,6 @@
 return {
   "nvim-treesitter/nvim-treesitter",
   opts = {
-    ensure_installed = { "yaml", "python", "typescript" },
+    ensure_installed = { "yaml", "python", "javascript", "typescript" },
   },
 }
