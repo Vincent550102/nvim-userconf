052d407cd6256275291129af83a8bfaf60dbe831
Vincent550102 <5020559@gmail.com>
2023-08-16 10:52:10 +0800

    update color

--- init.lua
+++ init.lua
@@ -18,7 +18,7 @@ return {
   },
 
   -- Set colorscheme to use
-  colorscheme = "astrodark",
+  colorscheme = "sonokai",
 
   -- Diagnostics configuration (for vim.diagnostics.config({...})) when diagnostics are on
   diagnostics = {
